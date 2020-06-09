8bd0157547ecf34c829b421b9700c76c6332fe7f
Robert Nsiah <robert.kofi.nsiah@gmail.com>
2020-06-09 03:35:01 +0000

    Answer to all 4

--- homework.js
+++ homework.js
@@ -1,25 +1,64 @@
+let fruit = 6;
 // Problem 1
 // declare a variable called fruit
 // assign your variable the value 6
 // console log your variable
 
+function product(num1, num2){
+  multiply = num1 * num2;
+  console.log(multiply);
+  return multiply
+}
 
+product(4,6)
 // write a function called product that accepts two numbers and 
 // logs the product in the console and then
 // returns the answer when you multiply them both together
 // call that function on the numbers 4 & 689 to test it
 
+function even(arr){
+  newArray=[]
+  for (val of arr ){
+    if(val % 2 === 0 ){
+      newArray.push(val) 
+     }
+     return newArray
+  }
+}
+even([4, 37, 18, 29, 52, 103, 22])
 // Problem 2
 // write a function called evens that takes an array of numbers, removes 
 // all the numbers that are odd and returns a new array with only the even numbers
 // remember that even numbers have a remainder of 0 when you divide them by 2
 // call your function on this array of numbers [4, 37, 18, 29, 52, 103, 22]
 
+
+function five(string){
+  console.log(string);
+  console.log(string);
+  console.log(string);
+  console.log(string);
+  console.log(string);
+}
+five('pineapple');
+
 //Problem 3
 // write a function called five that takes a word and prints it in the console
 // five times
 // call your function on the word "pineapple" to test it
 
+
+
+function shortwords(arrayOfWords){
+  let arrayOf3 = [];
+  for (let word of arrayOfWords){
+    if (word.length => 3){
+      arrayof3.push(word);
+    }
+    return arrayof3
+  }
+
+}
 //Problem 4
 // write a function called shortWords that takes an array of words and then
 // returns a new array with only the words that are 3 letters long or less
